27aa70f9c6d3b0cc316d10f7499cf077c9223f29
shu-kudan <shu@kudan.eu>
2026-06-10 15:20:23 +0900

    change setting for jetson

--- launch/start_no_rviz.py
+++ launch/start_no_rviz.py
@@ -2,8 +2,8 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
+
 def generate_launch_description():
-    rviz_config = get_package_share_directory('hesai_ros_driver') + '/rviz/rviz2.rviz'
     yaml_config = get_package_share_directory('hesai_ros_driver') + '/config/config.yaml'
     return LaunchDescription([
         Node(
@@ -13,11 +13,4 @@ def generate_launch_description():
             output='screen',
             parameters=[{'config_path': yaml_config}]
         ),
-        Node(
-            namespace='rviz2',
-            package='rviz2',
-            executable='rviz2',
-            arguments=['-d', rviz_config]
-        ),
     ])
-
